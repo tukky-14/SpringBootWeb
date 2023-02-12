cbc0969af166427587c0050e75a611a38a7b97ca
tukky-14 <bigoonage0104@gmail.com>
2023-02-12 16:30:53 +0900

    Modelのファイル追加

--- src/main/java/com/example/springbootweb/Register.java
+++ src/main/java/com/example/springbootweb/Register.java
@@ -2,8 +2,10 @@ package com.example.springbootweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.example.springbootweb.model.ModelTest;
 
 @Controller
 public class Register {
@@ -17,6 +19,18 @@ public class Register {
   public ModelAndView register(@ModelAttribute RegisterBean rb, ModelAndView m) {
     m.addObject("rb", rb);
     m.setViewName("register.html");
+    return m;
+  }
+
+  @PostMapping("/result")
+  public ModelAndView test(@ModelAttribute ModelTest modelTest, ModelAndView m) {
+    StringBuilder teststr = new StringBuilder();
+    teststr.append("name" + modelTest.getName());
+    teststr.append("age" + modelTest.getName());
+    teststr.append("remarks" + modelTest.getName());
+
+    m.setViewName("result");
+    m.addObject("resultData", teststr.toString());
     return m;
   }
 }
---
+++ src/main/java/com/example/springbootweb/model/ModelTest.java
@@ -0,0 +1,10 @@
+package com.example.springbootweb.model;
+
+import lombok.Data;
+
+@Data
+public class ModelTest {
+    private String name;
+    private String age;
+    private String remarks;
+}
